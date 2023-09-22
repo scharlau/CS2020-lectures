ba8d44d6d2eae117b94c404fac25c87176a520ac
BRUCE SCHARLAU_PreviewUser <b.scharlau@abdn.ac.uk>
2023-09-22 13:20:17 +0000

    second branch

--- week-one/HelloWorld.java
+++ week-one/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello");
-        System.out.println("World");
+        //System.out.printf("%s%n%s%n", "Hello", "world");
+        String hello = "Hello\nWorld\n";
+        System.out.print(hello);
+        hello = "Hello folks";
+       System.out.println(hello);
     }
 }
